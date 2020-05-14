d314d99d7b9dcd717c1128374a6ab8825843003f
momocus <momocus333@gmail.com>
2020-05-15 00:18:22 +0900

    Fix: #18

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module Sakazuki
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
+
+    # タイムゾーンの設定
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
