032c41df7b95e32cf6c6021aa7d7c3465ab39294
momocus <momocus333@gmail.com>
2020-07-12 14:57:39 +0900

    Fix #29 seeds.rbテンプレート更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,59 +7,60 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Sake.create!(
-  name: "", # 必須。空文字不可。
-  kura: "",
-  photo: nil,
-  bindume_date: DateTime.new(2020, 6, 1),
-  brew_year: DateTime.new(2020, 6, 1),
-  todofuken: "",
-  taste_value: nil, # integer 0 - 10
-  aroma_value: nil, # integer 0 - 10
-  nihonshudo: nil,
-  sando: nil,
-  aroma_impression: "",
-  color: "",
+  name: "x", # string, 1 or more characters
+  kura: "", # string
+  photo: nil, # 未実装
+  bindume_date: Date.new(2020, 6),
+  brew_year: Date.new(2020),
+  todofuken: "", # string
+  taste_value: nil, # nil or integer 0 - 10
+  aroma_value: nil, # nil or integer 0 - 10
+  nihonshudo: nil, # nil or integer
+  sando: nil, # nil or integer
+  aroma_impression: "", # string
+  color: "", # string
   taste_impression: "",
-  nigori: "",
-  awa: "",
-  tokutei_meisho: nil,
-  #   none: 0,
-  #   honjozo: 1,
-  #   ginjo: 2,
-  #   daiginjo: 3,
-  #   junmai: 4,
-  #   junmai_ginjo: 5,
-  #   junmai_daiginjo: 6,
-  #   tokubetsu_honjozo: 7,
-  #   tokubetsu_junmai: 8,
-  genryoumai: "",
-  kakemai: "",
-  kobo: "",
-  alcohol: nil,
-  aminosando: nil,
-  season: "",
-  warimizu: 0,
-  #   unknown: 0,
-  #   kasui: 1,
-  #   genshu: 2,
-  moto: nil,
-  #   unknown: 0,
-  #   kimoto: 1,
-  #   yamahai: 2,
-  #   sokujo: 3,
-  seimai_buai: nil,
-  roka: "",
-  shibori: "",
-  note: "",
-  bottle_level: 0,
-  #   sealed: 0,
-  #   opened: 1,
-  #   empty: 2,
-  hiire: 0,
-  #   unknown: 0,
-  #   namanama: 1,
-  #   mae_hiire: 2,
-  #   ato_hiire: 3,
-  #   nido_hiire: 4,
+  nigori: "", # string
+  awa: "", # string
+  tokutei_meisho: :none,
+  #   なし :none
+  #   本醸造 :honjozo
+  #   吟醸 :ginjo
+  #   大吟醸 :daiginjo
+  #   純米 :junmai
+  #   純米吟醸 :junmai_ginjo
+  #   純米大吟醸 :junmai_daiginjo
+  #   特別本醸造 :tokubetsu_honjozo
+  #   特別純米 :tokubetsu_junmai
+  genryoumai: "", # string
+  kakemai: "", # string
+  kobo: "", # string
+  alcohol: 15, # integer (nil禁止)
+  aminosando: nil, # nil or integer
+  season: "", # string
+  warimizu: :unknown,
+  #   不明 :unknown
+  #   加水あり :kasui
+  #   原酒（無加水） :genshu
+  moto: :unknown,
+  #   不明 :unknown
+  #   生酛 :kimoto
+  #   山廃 :yamahai
+  #   速醸 :sokujo
+  seimai_buai: nil, # nil or integer 0 - 10
+  roka: "", # string (e.g. 無濾過, 素濾過, 不明)
+  shibori: "", # string (e.g. 雫取り, 中組み, 不明)
+  note: "", # string
+  bottle_level: :sealed,
+  #   未開栓 :sealed
+  #   開封済 :opened
+  #   空 :empty
+  hiire: :unknown,
+  #   不明 :unknown
+  #   生生 :namanama
+  #   前火入れ :mae_hiire
+  #   あと火入れ :ato_hiire
+  #   二度火入れ :nido_hiire
   price: nil,
+  size: 0 # integer (nil禁止)
 )
