62c52113cd17118907841d2170a5d5e218ffc50e
SAITOU Keita <keita44.f4@gmail.com>
2020-07-12 17:50:39 +0900

    Refactor: Rubocop指摘事項を修正した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rails db:seed command (or created
+# alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' },
+#                          { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Sake.create!(
@@ -62,5 +65,5 @@ Sake.create!(
   #   あと火入れ :ato_hiire
   #   二度火入れ :nido_hiire
   price: nil,
-  size: 0 # integer (nil禁止)
+  size: 0, # integer (nil禁止)
 )
