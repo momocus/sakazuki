31d92579ca065b5f89d64929b756797d4308ad84
momocus <momocus333@gmail.com>
2020-07-23 15:42:35 +0900

    Add: User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_044802) do
+ActiveRecord::Schema.define(version: 2020_07_16_145736) do
 
   create_table "sakes", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2020_07_12_044802) do
     t.integer "hiire", default: 0
     t.integer "price"
     t.integer "size"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
