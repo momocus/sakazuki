df00bd8d946ab8dbc6f2c952a1a71cc506fb6ed1
momocus <momocus333@gmail.com>
2020-07-23 15:42:35 +0900

    Add: 登録されたユーザーの情報を表示するページ

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "sakes#index"
   get "/signup", to: "users#new"
+  resources :users
 end
