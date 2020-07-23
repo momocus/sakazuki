21eaa3e310ebec94757e28a8e520bff73916a586
momocus <momocus333@gmail.com>
2020-07-23 15:42:35 +0900

    Update: ユーザーモデルのemailにインデックスを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_145736) do
+ActiveRecord::Schema.define(version: 2020_07_22_080001) do
 
   create_table "sakes", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_07_16_145736) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
