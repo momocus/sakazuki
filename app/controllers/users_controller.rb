7671908b842e261f85f580b1dc224f285b0e0ecd
momocus <momocus333@gmail.com>
2020-07-24 17:44:40 +0900

    Update: サインインとサインアウトをできるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sakazuki!"
       redirect_to @user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    # rubocop:disable Rails/HelperInstanceVariable
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    # rubocop:enable Rails/HelperInstanceVariable
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def sign_out
+    session.delete(:user_id)
+    @current_user = nil # rubocop:disable Rails/HelperInstanceVariable
+  end
 end
