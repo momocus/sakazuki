79620b947c4ad3b6f9ccd27cc70a95597bc243a4
momocus <momocus333@gmail.com>
2020-07-24 17:44:40 +0900

    Add: サインインページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root "sakes#index"
+  get     "/signup",  to: "users#new"
+  get     "/signin",  to: "sessions#new"
+  post    "/signin",  to: "sessions#create"
+  delete  "/signout", to: "sessions#destroy"
   resources :sakes do
     get :filter, on: :collection
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "sakes#index"
-  get "/signup", to: "users#new"
   resources :users
 end
