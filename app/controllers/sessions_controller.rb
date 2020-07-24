fc70ca3e34d634a6d3f8e06520b45fb186a13a96
momocus <momocus333@gmail.com>
2020-07-24 17:44:40 +0900

    Add: セッションコントローラ

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render "new"
+    end
+  end
+
+  def destroy; end
+end
