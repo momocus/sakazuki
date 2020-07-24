93d0c5c2c9e551b27a415d8fcc03db1e786dec47
momocus <momocus333@gmail.com>
2020-07-24 18:02:24 +0900

    Add: Usersモデルにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_080608) do
+ActiveRecord::Schema.define(version: 2020_07_23_084108) do
 
   create_table "sakes", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_080608) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
