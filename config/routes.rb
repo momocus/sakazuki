62395b67a260050a2401ecb4e88a242d7295bfd5
momocus <momocus333@gmail.com>
2020-07-26 17:06:47 +0900

    SignUpを封じた

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "sakes#index"
-  get     "/signup",  to: "users#new"
+  # get     "/signup",  to: "users#new" 新規ユーザー登録はできないようにする
   get     "/signin",  to: "sessions#new"
   post    "/signin",  to: "sessions#create"
   delete  "/signout", to: "sessions#destroy"
