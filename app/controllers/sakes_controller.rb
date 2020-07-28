1c1630037c7304209dbb27a9370c9b231cd6bc7b
momocus <momocus333@gmail.com>
2020-07-28 18:19:20 +0900

    Fix: 酒の削除ができない問題

--- app/controllers/sakes_controller.rb
+++ app/controllers/sakes_controller.rb
@@ -1,7 +1,6 @@
 class SakesController < ApplicationController
   before_action :set_sake, only: %i[show edit update destroy]
   before_action :signed_in_user, only: %i[new create edit update destroy]
-  before_action :correct_user, only: :destroy
 
   # GET /sakes
   # GET /sakes.json
