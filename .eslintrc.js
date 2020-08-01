fec0d6b6fd005aa81e1178db6af7373845095ce0
SAITOU Keita <keita44.f4@gmail.com>
2020-08-01 15:12:48 +0900

    Add: TypeScript用プラグインとともにeslintを導入した
    - yarnでdev環境に対してeslintとそのプラグインを追加した
        - yarn add --dev @typescript-eslint/eslint-plugin eslint-config-standard eslint eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard @typescript-eslint/parser
    - eslintの設定ファイル.eslintrc.jsを作成した
        - yarn eslint --init
    - 使い方
        - yarn eslintやyarn run eslintで動かす
    - 設定
        - 対象はapp/javascript/pack/以下のJavaScriptとTypeScript
        - .eslintignoreでapplication.jsを除外している

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'standard',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+}
