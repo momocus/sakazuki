edbc2d56d8f75bddf48dab494d7af7d4bba16517
SAITOU Keita <keita44.f4@gmail.com>
2020-08-03 17:59:42 +0900

    Update: eslintにtypescriptの型Lintを追加した
    - pluginを2つ追加した
    - @typescript-eslint/recommendedの方はstandardとコンフリクトする可能性がる
        - コンフリクトが起きた場合は削除する予定

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,15 @@ module.exports = {
   },
   extends: [
     'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
   plugins: [
     '@typescript-eslint',
