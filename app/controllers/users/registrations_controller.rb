bfe9ae0d96b77697c184d2b783873783de68102e
momocus <momocus333@gmail.com>
2020-08-06 00:55:00 +0900

    Update: Sign upを封じた
    registrations_controllerを作成し、
    sign upをしようとしたら無条件でroot_urlに戻すよう
    before_actionを設定した。
    参考: deviseによるコントローラのカスタマイズ
    https://github.com/heartcombo/devise#configuring-controllers
    
    継承元クラスのbefore_actionによって、ログイン済のユーザーはサインアップできないようになっていた。
    このためAdminUserが他のユーザーを追加するという使い方に対応できなかった。
    
    参考: Devise::RegistrationsController （継承元クラス）
    https://www.rubydoc.info/github/heartcombo/devise/master/Devise/RegistrationsController
    https://github.com/heartcombo/devise/blob/master/app/controllers/devise/registrations_controller.rb

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,47 @@
+module Users
+  include ApplicationHelper
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :redirect_to_root, only: %i[new create]
+
+    # GET /resource/sign_up
+    def new
+      super
+    end
+
+    # POST /resource
+    def create
+      super
+    end
+
+    # GET /resource/edit
+    # def edit
+    #   super
+    # end
+
+    # PUT /resource
+    # def update
+    #   super
+    # end
+
+    # DELETE /resource
+    # def destroy
+    #   super
+    # end
+
+    # GET /resource/cancel
+    # Forces the session data which is usually expired after sign
+    # in to be expired now. This is useful if the user wants to
+    # cancel oauth signing in/up in the middle of the process,
+    # removing all OAuth session data.
+    # def cancel
+    #   super
+    # end
+
+    private
+
+    def redirect_to_root
+      flash[:danger] = "Permission denied."
+      redirect_to(root_url)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   root "sakes#index"
   resources :sakes do
     get :filter, on: :collection
