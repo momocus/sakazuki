6628824116e23c4e481aa80436c97e543e56d7fe
momocus <momocus333@gmail.com>
2020-08-06 00:54:59 +0900

    Fix: ユーザ・セッションのモデルやコントローラを削除したので、ルーティングも削除した

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root "sakes#index"
-  # get     "/signup",  to: "users#new" 新規ユーザー登録はできないようにする
-  get     "/signin",  to: "sessions#new"
-  post    "/signin",  to: "sessions#create"
-  delete  "/signout", to: "sessions#destroy"
   resources :sakes do
     get :filter, on: :collection
   end
-  resources :users
 end
