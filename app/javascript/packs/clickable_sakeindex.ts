57508d7f7225be8d103fe6f6d8744704857a593f
SAITOU Keita <keita44.f4@gmail.com>
2020-08-06 20:01:52 +0900

    Fix: undefinedチェックされていないのを修正

--- app/javascript/packs/clickable_sakeindex.ts
+++ app/javascript/packs/clickable_sakeindex.ts
@@ -4,9 +4,11 @@ const trsColl = document.getElementsByClassName(
 const trs = Array.from(trsColl)
 
 for (const tr of trs) {
-  const id: string = tr.dataset.sakeId
-  const newLocation = '/sakes/' + id
-  tr.onclick = () => {
-    window.location.assign(newLocation)
+  const id = tr.dataset.sakeId
+  if (id != null) {
+    const newLocation = '/sakes/' + id
+    tr.onclick = () => {
+      window.location.assign(newLocation)
+    }
   }
 }
