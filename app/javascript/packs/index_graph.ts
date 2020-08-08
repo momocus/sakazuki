c7287bb2e37b2ec21792691e034a3bac891a7e35
SAITOU Keita <keita44.f4@gmail.com>
2020-08-09 00:05:50 +0900

    Add: トップページの酒リストに味香グラフを表示した

---
+++ app/javascript/packs/index_graph.ts
@@ -0,0 +1,30 @@
+import { Chart } from 'chart.js'
+import { TasteGraph, toGraphPoint } from './taste_graph'
+
+function getDomValueFromCanvas(canvas: HTMLCanvasElement): Chart.Point {
+  const tasteS = canvas.dataset.tasteValue
+  const aromaS = canvas.dataset.aromaValue
+  // not (undefined or empty string)
+  if (tasteS && aromaS) {
+    const taste = parseInt(tasteS)
+    const aroma = parseInt(aromaS)
+    if (taste != NaN && aroma != NaN) return { x: taste, y: aroma }
+  }
+  // データがないなら範囲外を描く
+  return toGraphPoint({ x: -1, y: -1 })
+}
+
+const hasCanvasID = (elem: HTMLCanvasElement): boolean => {
+  const id = elem.getAttribute('id')
+  return id != null
+}
+
+// Main
+{
+  const canvases = Array.from(document.getElementsByTagName('canvas'))
+  const graphs = canvases.filter(hasCanvasID)
+  graphs.forEach((canvas) => {
+    const p = getDomValueFromCanvas(canvas)
+    new TasteGraph(canvas, p)
+  })
+}
