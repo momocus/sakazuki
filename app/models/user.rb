e533b408838f7f3e831ff0db830bf83f35963f27
momocus <momocus333@gmail.com>
2020-08-10 23:37:34 +0900

    Fix: timeoutをなくした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :lockable, :timeoutable, :trackable
+         :lockable, :trackable
 end
