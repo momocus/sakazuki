b5cdfcede0f98390d2256e2693e12f294f39a4af
momocus <momocus333@gmail.com>
2020-08-12 16:12:23 +0900

    Revert "Fix: timeoutをなくした"
    This reverts commit e533b408838f7f3e831ff0db830bf83f35963f27.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :lockable, :trackable
+         :lockable, :timeoutable, :trackable
 end
