e2f52b6e9406aa0fe15d9d8d72f219bad7c5e44c
iteduki <mistolteen0127@gmail.com>
2020-08-19 21:49:00 +0900

    bundle exec annotate

--- test/models/sake_test.rb
+++ test/models/sake_test.rb
@@ -1,3 +1,43 @@
+# == Schema Information
+#
+# Table name: sakes
+#
+#  id               :bigint           not null, primary key
+#  alcohol          :float
+#  aminosando       :float
+#  aroma_impression :text
+#  aroma_value      :integer
+#  awa              :string
+#  bindume_date     :date
+#  bottle_level     :integer          default("sealed")
+#  brew_year        :date
+#  color            :string
+#  genryoumai       :string
+#  hiire            :integer          default("unknown")
+#  kakemai          :string
+#  kobo             :string
+#  kura             :string
+#  moto             :integer          default("unknown")
+#  name             :string
+#  nigori           :string
+#  nihonshudo       :integer
+#  note             :text
+#  photo            :binary
+#  price            :integer
+#  roka             :string
+#  sando            :float
+#  season           :string
+#  seimai_buai      :integer
+#  shibori          :string
+#  size             :integer
+#  taste_impression :text
+#  taste_value      :integer
+#  todofuken        :string
+#  tokutei_meisho   :integer          default("none")
+#  warimizu         :integer          default("unknown")
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require "test_helper"
 
 class SakeTest < ActiveSupport::TestCase
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  admin                  :boolean          default(FALSE)
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unlock_token           :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
