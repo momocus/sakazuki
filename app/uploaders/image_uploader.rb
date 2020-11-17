ba4d00d24d37b2f3bde6cf98909bd5491b86a26f
momocus <momocus333@gmail.com>
2020-11-18 01:03:28 +0900

    Update: 拡張子のホワイトリストを追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,4 +13,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
 end
