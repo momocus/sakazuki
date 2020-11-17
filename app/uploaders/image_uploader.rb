b8077b3d1c4cc0c05231340955232783aea8f4d9
momocus <momocus333@gmail.com>
2020-11-18 01:03:28 +0900

    Update: 開発時はローカルディレクトリに画像を保存する

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,16 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    include CarrierWave::MiniMagick
+    storage :file
+  end
 
   version :thumb do
     process resize_to_fit: [100, 100]
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
