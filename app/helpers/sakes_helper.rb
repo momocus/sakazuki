1d422f878bf7d58ccb878373cf27ac4938ae3a8e
momocus <momocus333@gmail.com>
2020-11-26 15:37:52 +0900

    Update: 開発環境にCloudinary.ymlがなくても動くようにした

--- app/helpers/sakes_helper.rb
+++ app/helpers/sakes_helper.rb
@@ -3,4 +3,14 @@ module SakesHelper
   def empty_to_default(value, default = "-")
     value.nil? || value.blank? ? default : value
   end
+
+  # cl_image_tagメソッドはCloudinaryと連携した状態でないと動かないため、
+  # 本番環境とテスト環境で、画像表示のメソッドを切り替える
+  def wrap_image_tag(*args)
+    if Rails.env.production?
+      cl_image_tag(*args)
+    else
+      image_tag(*args)
+    end
+  end
 end
