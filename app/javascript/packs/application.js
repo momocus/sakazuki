c0971417e76276fec2e999c83089d781e35c8039
SAITOU Keita <keita44.f4@gmail.com>
2020-11-30 16:54:28 +0900

    Refactor: 各viewごとにwebpackerのエントリポイントを作成した
    今までのwebpackerのエントリポイントはapplication.jsのみだった。
    そのため各viewの末尾で必要なtypescriptファイルを読み込むのに、
    javascript_pack_tagを複数回呼び出していた。
    また、stylesheetがapp/javascriptとapp/assets/stylesheetに分散していた。
    
    このコミットでjavascriptのファイル構成を見直し、
    各viewごとのエントリポイントを用意した。
    各viewでは、自身のエントリポイント1つのみをjavascript_pack_tagで読み込めば良い。
    stylesheetはapp/javascript/src/stylesheetにまとめた。
    
    今後のファイル構成
    app/javascript
    ├─ channels (今回変更無し)
    ├─ packs
    │   ├─ application.js
    │   ├─ sakes_show.js
    │   ... (各view向けwebpackerエントリポイント)
    │
    └─ src
         ├── typescript
         │     ├─ taste_graph.ts
         │     ... (typescriptソースファイル)
         │
         └── stylesheet
                ├─ application.scss
                ... (stylesheetソースファイルcss/scss)
    
    また、このコミット以後は、app/javascript/src/stylesheet内にて、
    scssの@extendでbootstrapの内容を継承できるようになる。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 require("chart.js")
 
 import "bootstrap"
-import "../src/application.scss"
+import "../src/stylesheet/application.scss"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
