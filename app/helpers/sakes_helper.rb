10ef4659f6660a047cf458e625538e69f718839b
SAITOU Keita <keita44.f4@gmail.com>
2021-01-09 02:13:11 +0900

    Refactor: empty_to_default関数のデフォルト引数をやめた
    view特化のためにハイフンをデフォルト引数に指定していた。
    しかし、helper関数を他でも使い回すためにやめた。

--- app/helpers/sakes_helper.rb
+++ app/helpers/sakes_helper.rb
@@ -1,6 +1,6 @@
 module SakesHelper
-  # HACK: 整数値と文字列値のどちらでも使えるようにnil/blankで判定している
-  def empty_to_default(value, default = "-")
+  def empty_to_default(value, default)
+    # HACK: IntegerとStringのどちらでも使えるようにnil/blankで判定している
     value.nil? || value.blank? ? default : value
   end
 end
