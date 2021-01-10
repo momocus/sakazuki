8ff0fdb838802fad293bcc6ee3a9556302a5578d
momocus <momocus333@gmail.com>
2021-01-11 03:21:51 +0900

    Update: indexビューの画像サイズをレスポンシブにした
    - application.scssにthumb_imageスタイルを追加
      - img-fluidをextendして、親要素のcolに画像サイズを合わせるようにした
      - 親要素のcolにheight制限がなく、また、酒画像は縦長のものも多いので、height制限をつけた
    - 不要になったversion :thumb を削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,10 +6,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  version :thumb do
-    process resize_to_fit: [100, 100]
-  end
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
