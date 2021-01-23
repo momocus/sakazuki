a1e41d0f3c7854db3f188908e5ecf7f769751bf5
SAITOU Keita <keita44.f4@gmail.com>
2021-01-24 02:31:49 +0900

    Refactor: Railsの拡張機能を使い関数を簡素化した

--- app/helpers/sakes_helper.rb
+++ app/helpers/sakes_helper.rb
@@ -1,6 +1,5 @@
 module SakesHelper
   def empty_to_default(value, default)
-    # HACK: IntegerとStringのどちらでも使えるようにnil/blankで判定している
-    value.nil? || value.blank? ? default : value
+    value.presence || default
   end
 end
