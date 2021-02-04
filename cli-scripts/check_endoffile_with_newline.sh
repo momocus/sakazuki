849d0550ad41e1350d9494239f0488b45179b572
SAITOU Keita <keita44.f4@gmail.com>
2021-02-05 02:22:21 +0900

    Fix: スクリプトのtail/wcをショートオプションで使うようにした
    MacOSやFree BSDなどでは、ロングオプションが存在しない。

--- cli-scripts/check_endoffile_with_newline.sh
+++ cli-scripts/check_endoffile_with_newline.sh
@@ -16,7 +16,7 @@ error_files=""
 for file in ${files}; do
     # 改行で終わってないファイルを探し記録する
     # 参考: https://qiita.com/BlackCat_617/items/c0d7f7378bc55b3e07d0
-    if [ `tail --lines=1 ${file} | wc --lines` == 0 ]; then
+    if [ `tail -n 1 ${file} | wc -l` == 0 ]; then
         error_files="${error_files} ${file}"
     fi
 done
