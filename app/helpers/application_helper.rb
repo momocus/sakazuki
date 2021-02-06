f211f5139fdfd9f9a158b2b7b9bfd7272aba6102
SAITOU Keita <keita44.f4@gmail.com>
2021-02-06 16:19:03 +0900

    Update: 独自ヘルパーをやめてbootstrap-icons-helperを使うようにした
    PR #147 の指摘対応。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,4 @@ module ApplicationHelper
     display_meta_tags(twitter: { card: "summary" },
                       og: { title: "Sakazuki" })
   end
-
-  def bi_icon(icon, options = {})
-    path = asset_path("bootstrap-icons/bootstrap-icons.svg")
-    options = { class: "bi", fill: "currentColor" }.merge(options) { |_, default, added| "#{default} #{added}" }
-    tag.svg(tag.use(nil, "xlink:href" => "#{path}##{icon}"), options)
-  end
 end
