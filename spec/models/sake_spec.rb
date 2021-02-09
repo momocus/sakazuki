9bf933eb6b02a92e36addc7b13e4bae668b392dc
iteduki <mistolteen0127@gmail.com>
2021-02-09 22:17:15 +0900

    photoのテスト作成

---
+++ spec/models/sake_spec.rb
@@ -0,0 +1,44 @@
+# == Schema Information
+#
+# Table name: sakes
+#
+#  id               :bigint           not null, primary key
+#  alcohol          :float
+#  aminosando       :float
+#  aroma_impression :text
+#  aroma_value      :integer
+#  awa              :string
+#  bindume_date     :date
+#  bottle_level     :integer          default("sealed")
+#  brew_year        :date
+#  color            :string
+#  genryomai        :string
+#  hiire            :integer          default("unknown")
+#  kakemai          :string
+#  kobo             :string
+#  kura             :string
+#  moto             :integer          default("unknown")
+#  name             :string
+#  nigori           :string
+#  nihonshudo       :float
+#  note             :text
+#  price            :integer
+#  roka             :string
+#  sando            :float
+#  season           :string
+#  seimai_buai      :integer
+#  shibori          :string
+#  size             :integer
+#  taste_impression :text
+#  taste_value      :integer
+#  todofuken        :string
+#  tokutei_meisho   :integer          default("none")
+#  warimizu         :integer          default("unknown")
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+require "rails_helper"
+
+RSpec.describe Sake, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
