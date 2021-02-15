b9ecc0a8aa1e1e7629002ac2fe45033095ddc968
SAITOU Keita <keita44.f4@gmail.com>
2021-02-15 19:09:56 +0900

    Add: 酒のrequest specを作成した
    ログイン状態でのテストをするために、Deviseのヘルパーを設定した。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Devise helper for integration tests
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
---
+++ spec/requests/sake_request_spec.rb
@@ -0,0 +1,137 @@
+require "rails_helper"
+
+RSpec.describe "Sakes", type: :request do
+  let!(:sakes) { FactoryBot.create_list(:sake, 3) }
+  let(:id) { sakes[0].id }
+
+  context "without login" do
+    describe "GET /sakes request (index)" do
+      it "returns 200 response" do
+        get sakes_path
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    describe "GET /sakes.json request (index)" do
+      before do
+        get sakes_path(format: :json)
+      end
+
+      it "returns 200 response" do
+        expect(response).to have_http_status "200"
+      end
+
+      it "returns all sakes json" do
+        json = JSON.parse(response.body)
+        expect(json.present?).to be true
+      end
+    end
+
+    describe "GET /sakes/[id] request (show)" do
+      it "returns 200 response" do
+        get sake_path(id)
+        expect(response).to have_http_status "200"
+      end
+
+      it "returns 1 sake json" do
+        get sake_path(id, format: :json)
+        json = JSON.parse(response.body)
+        expect(json.present?).to be true
+      end
+    end
+
+    describe "GET /sakes/new request (new)" do
+      it "redirects to user login page" do
+        get new_sake_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "GET /sakes/[id]/edit (edit)" do
+      it "redirects to user login page" do
+        get edit_sake_path(id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "POST /sakes (create)" do
+      it "redirects to user login page" do
+        post sakes_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "GET /sakes/[id] (update)" do
+      it "redirects to user login page" do
+        patch sake_path(id, params: { sake: { bottle_level: "empty" } })
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "DELETE /sakes/[id] (destroy)" do
+      it "redirects to user login page" do
+        delete sake_path(id)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  context "with logined user" do
+    before do
+      user = FactoryBot.create(:user)
+      sign_in(user)
+    end
+
+    describe "GET /sakes/[id]/edit (edit)" do
+      it "returns 200 response" do
+        get edit_sake_path(id)
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    describe "POST /sakes (create)" do
+      # HACK: FactoryBotで作った適当なsakeを、JSON経由でhashにすることでparamsとして使う
+      sake_params = JSON.parse(FactoryBot.build(:sake).to_json)
+      sake_params.delete(:id)
+      sake_params.delete(:created_at)
+      sake_params.delete(:updated_at)
+
+      it "returns 302 response" do
+        post "/sakes", params: { sake: sake_params }
+        expect(response).to have_http_status "302" # redirect to "sakes/[new_id]"
+      end
+
+      it "increase 1 sake" do
+        expect { post "/sakes", params: { sake: sake_params } }.to change(Sake, :count).by 1
+      end
+    end
+
+    describe "GET /sakes/[id] (update)" do
+      let(:orig) { sakes[0].bottle_level }
+      let(:updated) { "empty" }
+
+      it "redirects to showing updated sake" do
+        patch sake_path(id, params: { sake: { bottle_level: updated } })
+        expect(response).to redirect_to sake_path(id)
+      end
+
+      it "updates sake state with patched params" do
+        expect do
+          patch sake_path(id, params: { sake: { bottle_level: updated } })
+          sakes[0].reload
+        end.to change(sakes[0], :bottle_level).from(orig).to(updated)
+      end
+    end
+
+    describe "DELETE /sakes/[id] (destroy)" do
+      it "redirects to index page" do
+        delete sake_path(id)
+        expect(response).to redirect_to sakes_path
+      end
+
+      it "decrease 1 sake" do
+        expect { delete sake_path(id) }.to change(Sake, :count).by(-1)
+      end
+    end
+  end
+end
