01724bd3aaabf60be6db062b733203e8af509ed0
momocus <momocus333@gmail.com>
2021-02-16 16:23:50 +0900

    Fix: わかりにくい環境変数名を変更
    ELASTICSEARCH_HOSTとELASTICSEARCH_HOSTSが似ていてわかりづらかった。
    kibanaのための環境変数ELASTICSEARCH_HOSTSは名前を変えられないため、
    ELASTICSEARCH_HOSTをELASTICSEARCH_HOSTNAME に変更した。

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 config = {
-  host: ENV["ELASTICSEARCH_HOST"] || "localhost",
+  host: ENV["ELASTICSEARCH_HOSTNAME"] || "localhost",
   port: ENV["ELASTICSEARCH_PORT"] || "9200",
 }
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
