36f61220675aabc24afb59c4320beb5b64d8a36b
momocus <momocus333@gmail.com>
2021-03-13 14:36:55 +0900

    indexer修正

--- app/workers/indexer.rb
+++ app/workers/indexer.rb
@@ -9,20 +9,20 @@ class Indexer
   sidekiq_options queue: "elasticsearch", retry: false
 
   LOGGER = Sidekiq.logger.level == Logger::DEBUG ? Sidekiq.logger : nil
-  CLIENT = Elasticsearch::Client.new(
+  Client = Elasticsearch::Client.new(
     url: ENV["ELASTICSEARCH_HOSTS"] || "http://localhost:9200",
     logger: LOGGER,
   )
 
-  def perform_async(operation, record_id)
+  def perform(operation, record_id)
     logger.debug([operation, "ID: #{record_id}"])
 
     case operation.to_s
     when /index/
       record = Sake.find(record_id)
-      Client.index({ index: "sakes", type: "sake", id: record.id, body: record.__elasticsearch__.as_indexed_json })
+      Client.index({ index: "sakes", id: record.id, body: record.__elasticsearch__.as_indexed_json })
     when /delete/
-      Client.delete({ index: "sakes", type: "sake", id: record_id })
+      Client.delete({ index: "sakes", id: record_id })
     else
       raise(ArgumentError, "Unknown operation #{operation}")
     end
