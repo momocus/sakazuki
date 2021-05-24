68a0c12da10eef2c1d7799d9ce0479a6f0b24550
SAITOU Keita <keita44.f4@gmail.com>
2021-05-24 23:13:37 +0900

    Revert "Refactor: 各viewページごとにスタイルシートのエントリーポイントを作った"
    This reverts commit fefc84e3f5f9f1d9bf16ed9fb081c0741124e0e4.
    
    本番環境でスタイルが崩れるのでリバート。
    2つのエントリーポイント（例えばapplication.jsとsakes_index.js）があると、
    片方が読み込まれないことがある。
    原因はよくわからなかった。

--- app/javascript/packs/sakes_form.js
+++ app/javascript/packs/sakes_form.js
@@ -1,4 +1,3 @@
-import '../src/stylesheet/sakes_form.scss'
 import '../src/typescript/autocomplete_todofuken.ts'
 import '../src/typescript/async_bindume_date.ts'
 import '../src/typescript/input_taste_graph.ts'
--- app/javascript/packs/sakes_show.js
+++ app/javascript/packs/sakes_show.js
@@ -1,2 +1,1 @@
-import '../src/stylesheet/sakes_show.scss'
 import '../src/typescript/show_taste_graph.ts'
