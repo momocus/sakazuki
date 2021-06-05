8b05e809450545931d3dc0f1eab6843e4b75cef2
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-06-05 22:10:06 +0900

    Chore: 自動修正できなかったものを修正. See https://www.rubydoc.info/gems/rubocop-rails/RuboCop/Cop/Rails/Output

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
+  Rails.logger.debug e.to_s.strip
   exit 1
 end
 RSpec.configure do |config|
