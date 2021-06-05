9359b422fb2e3699c53811c40f2821a920ba5d48
SAITOU Keita <keita44.f4@gmail.com>
2021-06-06 04:23:40 +0900

    Fix: 同期処理にasyncって名前つけてたtypoを修正

--- app/javascript/packs/sakes_form.js
+++ app/javascript/packs/sakes_form.js
@@ -1,3 +1,3 @@
 import '../src/typescript/autocomplete_todofuken.ts'
-import '../src/typescript/async_bindume_date.ts'
+import '../src/typescript/sync_bindume_date.ts'
 import '../src/typescript/input_taste_graph.ts'
