c17bf5c96345a51ca9cf6570000494880af0411f
momocus <momocus333@gmail.com>
2021-06-13 21:10:51 +0900

    Update: ログイン時にインデクスに戻されないようにした
    参考:
    https://github.com/heartcombo/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::Base
+  after_action :store_location
+
   def signed_in_user
     return if user_signed_in?
 
+    # 未ログインユーザーが酒作成・編集しようとしたときにパスを保存する
+    store_location
     flash[:danger] = "Please sign in."
     redirect_to(new_user_session_path)
+  end
+
+  # 以下の場合にユーザーが居たパスを保存する
+  # - リクエストがGETメソッドである。
+  # - Ajaxリクエストではない。
+  # - ユーザー認証ページへのパスではない。
+  #   （リダイレクトループが発生するのを避けるため）
+  def store_location
+    if request.get? &&
+       !request.xhr? &&
+       !request.path.match?("\/users\/.*")
+      store_location_for(:user, request.fullpath)
+    end
   end
 end
