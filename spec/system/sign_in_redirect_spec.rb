5c3080387111d987ee37f3f4c4a3bce76e5b6906
momocus <momocus333@gmail.com>
2021-06-14 22:06:09 +0900

    Fix: 英文を修正
    - レビュー指摘対応
    - when user signs in from header button on sign in pageはリダイレクトループが発生しないことを確認するのが主目的であるためカッコで追記した。

--- spec/system/sign_in_redirect_spec.rb
+++ spec/system/sign_in_redirect_spec.rb
@@ -10,56 +10,56 @@ RSpec.describe "Sign-in redirect", type: :system do
     sign_out(user)
   end
 
-  context "when non-signed-in user tries to edit-sake" do
+  context "when not signed user tries to edit sake" do
     before do
       visit sake_path(id)
       find(:test_id, "edit-#{id}").click
     end
 
-    it "redirects user to new_user_session_path" do
+    it "redirects to sign in page" do
       expect(page).to have_current_path new_user_session_path
     end
 
-    it "redirect to edit_sake_path after user sign-in" do
+    it "redirect to sake editing page after sign in" do
       signin_process_on_signin_page(user)
       expect(page).to have_current_path edit_sake_path(id)
     end
   end
 
-  context "when non-signed-in user tries to new-sake" do
+  context "when not signed user tries to create sake" do
     before do
       visit sakes_path
       find(:test_id, "new-sake").click
     end
 
-    it "redirects user to new_user_session_path" do
+    it "redirects to sign in page" do
       expect(page).to have_current_path new_user_session_path
     end
 
-    it "redirects user to new_sake_path after user sign-in" do
+    it "redirects to sake creation page after sign in" do
       signin_process_on_signin_page(user)
       expect(page).to have_current_path new_sake_path
     end
   end
 
-  context "when user presses sign-in button on sake show page" do
+  context "when user signs in from header button on sake show page" do
     before do
       visit sake_path(id)
       sign_in_via_header_button(user)
     end
 
-    it "redirects user to sake show page after user sign-in" do
+    it "redirects to sake show page" do
       expect(page).to have_current_path sake_path(id)
     end
   end
 
-  context "when user presses sign-in button on sign-in page" do
+  context "when user signs in from header button on sign in page" do
     before do
       visit new_user_session_path
       sign_in_via_header_button(user)
     end
 
-    it "redirects user to root_path after user sign-in" do
+    it "redirects user to root url (does not occur redirection loop)" do
       expect(page).to have_current_path root_path
     end
   end
