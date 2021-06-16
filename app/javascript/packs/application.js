64b45daa419747bbae5686a5f2e3b3aa484dea90
SAITOU Keita <keita44.f4@gmail.com>
2021-06-16 15:35:13 +0900

    Refactor: application.jsのrequireをRails6の書き方であるimportに変更した
    Rails v6.1.3.2でrails new blogすると、この書き方をいたapplication.jsが自動生成された。
    よって採用した。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,14 @@
 
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 /* eslint-enable @typescript-eslint/no-unsafe-member-access */
 /* eslint-enable @typescript-eslint/no-unsafe-call */
 
