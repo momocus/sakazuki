013b11ac5a4407ab437907fea8ef8b59451496f2
SAITOU Keita <keita44.f4@gmail.com>
2021-06-19 15:03:07 +0900

    Refactor: capybara helper methodのコメントをYARDにした

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,13 +3,12 @@ require "capybara/rspec"
 Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :selenium_headless
 
+# ページ遷移を待つ
+#
+# Capybaraのfindはページロードを待つため、これを使えばもっと効率のよいwaitが実装できる。
+# しかし、findでページ遷移をwaitするには、遷移前と遷移後で変化する要素が必要となる
+# 任意のページ遷移で該当する要素が思いつかず、汎用関数にできていない。
 def wait_for_page
-  # MEMO:
-  # findなどを使って実装しようとしたが諦めた。
-  # Capybaraはfindが失敗すると自動でページ遷移を待つ機能がある。
-  # よって、findでページ遷移を待つためには、
-  # 遷移前と遷移後で変化する要素を指定する必要がある。
-  # しかし、すべてのページ遷移で変化する要素が思いつかず、汎用関数にできなかった。
   sleep(1)
   nil
 end
