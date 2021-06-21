68189c2f41e53f2c348e65d20da525f2b916ecd9
SAITOU Keita <keita44.f4@gmail.com>
2021-06-21 12:36:20 +0900

    Refactor: テストの共通項をbeforeで切り出した
    PR #213 対応。

--- spec/system/signin_redirect_integrating_drinkbutton_spec.rb
+++ spec/system/signin_redirect_integrating_drinkbutton_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe "Sign in redirect with drink buttons", type: :system do
   let!(:impressed_sake) { FactoryBot.create(:sake, bottle_level: "opened", taste_value: 1, aroma_value: 2) }
   let(:user) { FactoryBot.create(:user) }
 
+  before do
+    sign_out(user)
+    visit sakes_path
+  end
+
   describe "impress button" do
     it "redirects edit page after sign in" do
-      sign_out(user)
-      visit sakes_path
       id = sealed_sake.id
       click_link("open-and-impress-button-#{id}")
       signin_process_on_signin_page(user)
@@ -19,8 +22,6 @@ RSpec.describe "Sign in redirect with drink buttons", type: :system do
 
   describe "open button of sealed sake" do
     it "redirects index page after sign in", js: true do
-      sign_out(user)
-      visit sakes_path
       id = sealed_sake.id
       accept_confirm do
         click_link("open-button-#{id}")
@@ -33,8 +34,6 @@ RSpec.describe "Sign in redirect with drink buttons", type: :system do
 
   describe "empty button" do
     it "redirects index page after sign in", js: true do
-      sign_out(user)
-      visit sakes_path
       id = impressed_sake.id
       accept_confirm do
         click_link("empty-button-#{id}")
