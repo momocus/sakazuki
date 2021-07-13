112f773bd44af0ace24edda79fca9c84b2cb4133
momocus <momocus333@gmail.com>
2021-07-13 20:03:50 +0900

    Add: 酒edit後のリダイレクト・フラッシュメッセージ表示のテスト

---
+++ spec/system/sake_update_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "AfterUpdateAction", type: :system do
+
+  let!(:opened_sake) { FactoryBot.create(:sake, bottle_level: "opened") }
+  let(:sake_id) { opened_sake.id }
+  let(:sake_name) { opened_sake.name }
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "update from edit page" do
+    before do
+      sign_in(user)
+      visit edit_sake_path(sake_id)
+      find(:test_id, "submit").click
+    end
+
+    it "redirect to sake page" do
+      expect(page).to have_current_path sake_path(sake_id)
+    end
+
+    it "has success flash message" do
+      expect(page).to have_css(".alert-success")
+    end
+
+    it "has link to the updated sake" do
+      expect(find(".alert")).to have_link(sake_name, href: sake_path(sake_id))
+    end
+  end
+end
