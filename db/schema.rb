def5c525ff83c7e2893dd8fa29294b177e2570dc
momocus <momocus333@gmail.com>
2021-09-08 17:01:54 +0900

    Update: userモデルをマイグレート
    DeviseInvitableに必要なカラムを追加した
    参考: https://github.com/scambra/devise_invitable#label-ActiveRecord+Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_072328) do
+ActiveRecord::Schema.define(version: 2021_08_29_172516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,15 @@ ActiveRecord::Schema.define(version: 2021_01_26_072328) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.integer "invited_by_id"
+    t.string "invited_by_type"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
