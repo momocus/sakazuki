97c4a280c1d91c9fb63e557da1c27e76fffb1a92
momocus <momocus333@gmail.com>
2021-09-08 17:01:54 +0900

    Update: コントローラを追加

---
+++ app/controllers/users/InvitationsController.rb
@@ -0,0 +1,15 @@
+module Users
+  class InvitationsController < Devise::InvitationsController
+    before_action :accept_admin, only: %i[new create]
+
+    private
+
+    # 招待メールを送れるのはadmin限定とする
+    def accept_admin
+      return if current_user.admin
+
+      flash[:danger] = t("controllers.user.permission_denied")
+      redirect_to(root_url)
+    end
+  end
+end
