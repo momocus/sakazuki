03bc5ddd02517347209df427c27b2b31ac5ca6ba
SAITOU Keita <keita44.f4@gmail.com>
2021-09-15 18:10:44 +0900

    Update: 味・香りソートで未評価（NULL）を最小値と扱うようにした
    Ransackの時期バージョンが出たら:nulls_always_lastを使いたい

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -2,4 +2,9 @@ Ransack.configure do |c|
   # Raise errors if a query contains an unknown predicate or attribute.
   # Default is true (do not raise error on unknown conditions).
   c.ignore_unknown_conditions = false
+
+  # ソート時NULL値を一番小さな数値として扱う
+  # TODO: Ransackのバージョンが上がったら:nulls_always_last（常に最後）を採用する
+  c.postgres_fields_sort_option = :nulls_first
+  # c.postgres_fields_sort_option = :nulls_always_last
 end
