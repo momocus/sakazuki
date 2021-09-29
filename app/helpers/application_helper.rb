fdeefd4619429a2a9047fc8c194d1743142a77ba
SAITOU Keita <keita44.f4@gmail.com>
2021-09-29 17:48:26 +0900

    Refactor: SEOメタタグをメソッドに切り出した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def seo_meta_tags
+    display_meta_tags(site: "SAKAZUKI",
+                      title: "",
+                      separator: "-",
+                      reverse: true,
+                      description: t(".description"),
+                      og: {
+                        title: :title,
+                        url: request.original_url,
+                        type: "website",
+                        image: asset_pack_path("media/images/SAKAZUKI_choko.svg"),
+                        description: :description,
+                      },
+                      twitter: { card: "summary" },
+                      fb: { app_id: ENV["FB_APP_ID"] })
+  end
 end
