8c7c459babc5427ef6ac620c2878df6f7dbc1108
momocus <momocus333@gmail.com>
2021-10-12 17:53:34 +0900

    Fix: jsファイルのimportを相対パスにした

--- app/packs/entrypoints/common.js
+++ app/packs/entrypoints/common.js
@@ -25,7 +25,7 @@ import "../src/javascript/bootstrap_plugins.js"
 import "bootstrap-icons/font/bootstrap-icons"
 
 // image
-import "images/choko.svg"
+import "../images/choko.svg"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
