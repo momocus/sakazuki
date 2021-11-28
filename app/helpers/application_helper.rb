4cfa68f26963667004706ad9076f844711547241
SAITOU Keita <keita44.f4@gmail.com>
2021-11-28 18:48:30 +0900

    Refactor: Rubocopエラーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,33 @@
 module ApplicationHelper
   # rubocop:disable Metrics/MethodLength
   def seo_meta_tags
-    set_meta_tags(
-      icon: [
-        {
-          rel: "apple-touch-icon-precomposed",
-          sizes: "180x180",
-          href: asset_pack_path("media/images/apple-touch-icon.png"),
-          type: "image/png",
-        },
-        {
-          rel: "icon",
-          sizes: "32x32",
-          href: asset_pack_path("media/images/favicon-32x32.png"),
-          type: "image/png",
-        },
-      ],
-      site: "SAKAZUKI",
-      title: "",
-      separator: "-",
-      reverse: true,
-      description: t(".description"),
-      og: {
-        title: :title,
-        url: request.original_url,
-        type: "website",
-        image: asset_pack_path("media/images/choko.svg"),
-        description: :description,
-      },
-      twitter: { card: "summary" },
-    )
+    set_meta_tags(icon: [
+                    {
+                      rel: "apple-touch-icon-precomposed",
+                      sizes: "180x180",
+                      href: asset_pack_path("media/images/apple-touch-icon.png"),
+                      type: "image/png",
+                    },
+                    {
+                      rel: "icon",
+                      sizes: "32x32",
+                      href: asset_pack_path("media/images/favicon-32x32.png"),
+                      type: "image/png",
+                    },
+                  ],
+                  site: "SAKAZUKI",
+                  title: "",
+                  separator: "-",
+                  reverse: true,
+                  description: t(".description"),
+                  og: {
+                    title: :title,
+                    url: request.original_url,
+                    type: "website",
+                    image: asset_pack_path("media/images/choko.svg"),
+                    description: :description,
+                  },
+                  twitter: { card: "summary" })
     set_meta_tags(fb: { app_id: ENV["FB_APP_ID"] }) if ENV["FB_APP_ID"]
     display_meta_tags
   end
