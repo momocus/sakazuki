a5bb674533421632ebafe12bf883b9dcd3da272f
SAITOU Keita <keita44.f4@gmail.com>
2022-03-01 17:22:04 +0900

    Remove: Rails6.0の設定を削除しRails6.1の設定を使うようにした

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,6 @@ Bundler.require(*Rails.groups)
 
 module Sakazuki
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults(6.0)
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
