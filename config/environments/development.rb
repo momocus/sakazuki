1ae3b14f595678d1b404eb5bf009272e88ca9dfc
SAITOU Keita <keita44.f4@gmail.com>
2022-03-06 01:52:05 +0900

    Refactor: 必要ないRubocop disableを削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 require "active_support/core_ext/integer/time"
 
-# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -72,4 +71,3 @@ Rails.application.configure do
   # DockerでBetterErrorsを動かすための設定
   BetterErrors::Middleware.allow_ip!("0.0.0.0/0")
 end
-# rubocop:enable Metrics/BlockLength
