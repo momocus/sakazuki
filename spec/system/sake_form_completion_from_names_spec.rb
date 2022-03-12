cd6f5b33b19d640157fdcbe217be6c4a2252a189
SAITOU Keita <keita44.f4@gmail.com>
2022-03-12 17:09:06 +0900

    Add: 酒の名前から銘柄によって蔵名を補完するRSpecを書いた

---
+++ spec/system/sake_form_completion_from_names_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "SakeFormCompletionFromNames", type: :system do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+    visit new_sake_path
+  end
+
+  describe "kura_todofuken", js: true do
+    context "with empty kura_todofuken" do
+      it "is completed" do
+        # "send_keys(:tab)" is to trigger change event.
+        # https://github.com/teamcapybara/capybara/issues/2105
+        fill_in("sake_name", with: "生道井 本醸造 無ろ過生原酒 袋吊り").send_keys(:tab)
+        expect(find(:test_id, "sake_kura_todofuken_autocompletion").value).to eq("原田酒造合資会社（愛知県）")
+      end
+    end
+
+    context "with filled kura_todofuken" do
+      before do
+        fill_in("sake_kura_todofuken_autocompletion", with: "丸一酒造株式会社")
+      end
+
+      it "is not completed" do
+        fill_in("sake_name", with: "生道井 本醸造 無ろ過生原酒 袋吊り").send_keys(:tab)
+        expect(find(:test_id, "sake_kura_todofuken_autocompletion").value).to eq("丸一酒造株式会社")
+      end
+    end
+
+    context "with duplicate meigara" do
+      it "is not completed" do
+        # 黒龍は、栃木県の太平酒造と、福井県の黒龍酒造との2つある
+        fill_in("sake_name", with: "黒龍").send_keys(:tab)
+        expect(find(:test_id, "sake_kura_todofuken_autocompletion").value).to eq("")
+      end
+    end
+  end
+end
