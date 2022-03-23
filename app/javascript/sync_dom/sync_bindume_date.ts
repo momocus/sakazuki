2144d9c6179bc00527fd62865db31cac76c33774
SAITOU Keita <keita44.f4@gmail.com>
2022-03-23 16:10:02 +0900

    Update: 酒フォームの製造年月の2つのダミーを同期するようにした

--- app/javascript/sync_dom/sync_bindume_date.ts
+++ app/javascript/sync_dom/sync_bindume_date.ts
@@ -1,18 +1,37 @@
-function syncElement(srcId: string, dstId: string) {
+function syncElement(srcId: string, dstId: string): void {
   const srcElement = document.getElementById(srcId) as HTMLSelectElement
   const dstElement = document.getElementById(dstId) as HTMLSelectElement
-  srcElement?.addEventListener("change", (_event) => {
-    dstElement.value = srcElement.value
+  dstElement.value = srcElement.value
+}
+
+function setSyncEvent(): void {
+  // year
+  const yearElement = document.getElementById(
+    "sake_bindume_year"
+  ) as HTMLSelectElement
+  yearElement.addEventListener("change", (_event) => {
+    syncElement("sake_bindume_year", "sake_bindume_date_1i")
+  })
+  // month
+  const monthElement = document.getElementById(
+    "sake_bindume_month"
+  ) as HTMLSelectElement
+  monthElement.addEventListener("change", (_event) => {
+    syncElement("sake_bindume_month", "sake_bindume_date_2i")
   })
 }
 
-function setBindumeDateEvent() {
-  syncElement("bindume_year", "sake_bindume_date_1i")
+function loadBindumeDate() {
+  syncElement("sake_bindume_date_1i", "sake_bindume_year")
+  syncElement("sake_bindume_date_2i", "sake_bindume_month")
 }
 
 // Main
 {
   document.addEventListener("DOMContentLoaded", function () {
-    setBindumeDateEvent()
+    loadBindumeDate()
+    setSyncEvent()
   })
 }
+
+export {}
