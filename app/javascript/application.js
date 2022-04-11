7f6fc24d8cb7cb6626ca2af6e61148a8f55dd231
SAITOU Keita <keita44.f4@gmail.com>
2022-04-12 01:46:59 +0900

    Update: @rails/ujsがnode形式exportしているのでrequireを使う

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
-import Rails from "@rails/ujs"
+/* eslint-env node */
+const Rails = require("@rails/ujs")
 Rails.start()
 
 import "./bootstrap_plugins/bootstrap_plugins.js"
