cd963d0273b4292c27caabf33ec21c9e7f540e90
SAITOU Keita <keita44.f4@gmail.com>
2022-04-16 18:53:47 +0900

    Remove: dummyの製造年月日をやめた
    今まではdummyのinputフォームを並べることで、横2列のdateセレクタを実現していた。
    しかし、stylesheetの指定のみで解決が可能なことがわかった。
    そのため今までのTSを使った複雑なやりかたを削除した。
    
    参考
    https://qiita.com/t_oginogin/items/519fb52e1708e26a8b73

--- app/javascript/sakes_form.js
+++ app/javascript/sakes_form.js
@@ -1,5 +1,4 @@
 import "./autocompletion/complete_meigara.ts"
 import "./sync_dom/sync_kura_todofuken.ts"
-import "./sync_dom/sync_bindume_date.ts"
 import "./taste_graph/input_taste_graph.ts"
 import "./rating/rating.ts"
