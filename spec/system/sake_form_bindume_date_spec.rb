ae897c5490d90c0c2dfc81d689d5a8f4282c50af
SAITOU Keita <keita44.f4@gmail.com>
2022-04-17 03:51:50 +0900

    Update: 製造日時とBYのSpecを分離した
    今まではまとめて同じファイルに入っていた。
    しかし酒フォームのSpecは各フォーム項目ごとに分けておくほうがわかりやすい。
    そのためこの2つを分離したファイルに配置し、テスト内容を見直した。

--- spec/system/sake_form_bindume_date_spec.rb
+++ spec/system/sake_form_bindume_date_spec.rb
@@ -8,64 +8,118 @@ RSpec.describe "Sake Form Bindume Date", type: :system do
 
   before do
     sign_in user
-    visit new_sake_path
-    fill_in("sake_name", with: "生道井")
   end
 
-  describe "year of bindume_date", js: true do
-    context "when select current year" do
+  describe "year of bindume_date" do
+    context "when visiting new sake page" do
       before do
+        visit new_sake_path
+      end
+
+      it "selects this year" do
+        year = with_japanese_era(Time.current)
+        expect(page).to have_select("sake_bindume_date_1i", selected: year)
+      end
+    end
+
+    context "when creating new sake with current time" do
+      before do
+        visit new_sake_path
+        fill_in("sake_name", with: "生道井")
         year = with_japanese_era(Time.current)
         select(year, from: "sake_bindume_date_1i")
         click_button("form_submit")
       end
 
-      it "makes a sake whoes bindume year is current year" do
+      it "has current year" do
         sake = sake_from_show_path(page.current_path)
         expect(sake.bindume_date.year).to eq(Time.current.year)
       end
     end
 
-    context "when select 10 years ago" do
+    context "when creating new sake with past time" do
       ago = Time.current.ago(10.years)
 
       before do
+        visit new_sake_path
+        fill_in("sake_name", with: "生道井")
         year = with_japanese_era(ago)
         select(year, from: "sake_bindume_date_1i")
         click_button("form_submit")
       end
 
-      it "makes a sake whoes bindume year is 10 years ago" do
+      it "has past year" do
         sake = sake_from_show_path(page.current_path)
         expect(sake.bindume_date.year).to eq(ago.year)
       end
     end
+
+    context "when visiting edit sake page" do
+      let(:sake) { create(:sake, bindume_date: Date.parse("2021-07-01")) }
+
+      before do
+        visit edit_sake_path(sake.id)
+      end
+
+      it "selects the year of sake" do
+        year = with_japanese_era(sake.bindume_date)
+        expect(page).to have_select("sake_bindume_date_1i", selected: year)
+      end
+    end
   end
 
-  describe "month of bindume_date", js: true do
-    context "when select January" do
+  describe "month of bindume_date" do
+    context "when visiting new sake page" do
       before do
+        visit new_sake_path
+      end
+
+      it "selected this month" do
+        month = I18n.l(Time.current, format: "%b")
+        expect(page).to have_select("sake_bindume_date_2i", selected: month)
+      end
+    end
+
+    context "when creating new sake with January" do
+      before do
+        visit new_sake_path
+        fill_in("sake_name", with: "生道井")
         month = I18n.l(Time.zone.parse("2022-01-01 10:30:00"), format: "%b")
         select(month, from: "sake_bindume_date_2i")
         click_button("form_submit")
       end
 
-      it "makes a sake whoes bindume month is January" do
+      it "has January" do
         sake = sake_from_show_path(page.current_path)
         expect(sake.bindume_date.month).to eq(1)
       end
     end
 
-    context "when select December" do
+    context "when creating new sake with December" do
       before do
+        visit new_sake_path
+        fill_in("sake_name", with: "生道井")
         month = I18n.l(Time.zone.parse("2022-12-01 10:30:00"), format: "%b")
         select(month, from: "sake_bindume_date_2i")
         click_button("form_submit")
       end
 
-      it "makes a sake whoes bindume month is December" do
+      it "has December" do
         sake = sake_from_show_path(page.current_path)
         expect(sake.bindume_date.month).to eq(12)
+      end
+    end
+
+    context "when visiting edit sake page" do
+      let(:sake) { create(:sake, bindume_date: Date.parse("2021-07-01")) }
+
+      before do
+        visit edit_sake_path(sake.id)
+      end
+
+      it "selects the month of sake" do
+        month = I18n.l(sake.bindume_date, format: "%b")
+        expect(page).to have_select("sake_bindume_date_2i", selected: month)
       end
     end
   end
