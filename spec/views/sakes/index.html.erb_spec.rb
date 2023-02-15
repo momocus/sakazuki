091a9e97548263aff131270bc03a3512b429bee5
SAITOU Keita <keita44.f4@gmail.com>
2023-02-15 15:03:44 +0900

    Add: 酒indexのタイトルに関するビューテストを追加

--- spec/views/sakes/index.html.erb_spec.rb
+++ spec/views/sakes/index.html.erb_spec.rb
@@ -2,19 +2,45 @@ require "rails_helper"
 
 # Capybaraを使うためにsystem specを指定する
 RSpec.describe "sakes/index", type: :system do
-  let!(:sake) { create(:sake) }
+  let!(:sake) { create(:sake, size: 720) }
 
-  describe "index page" do
-    before do
-      visit sakes_path
+  before do
+    visit sakes_path
+  end
+
+  describe "sake" do
+    it "has link to edit" do
+      buttons = "sake_buttons_#{sake.id}"
+      text = I18n.t("sakes.sake.edit")
+      path = edit_sake_path(sake.id)
+      expect(find(:test_id, buttons)).to have_link(text, href: path)
+    end
+  end
+
+  describe "title" do
+    it "has title with total amount of sake" do
+      title = "#{I18n.t('sakes.index.title')} - 4合"
+      expect(page).to have_text(title)
+    end
+  end
+
+  describe "title meta tag" do
+    context "when not searching" do
+      it "has title" do
+        title = "#{I18n.t('sakes.index.title')} - SAKAZUKI"
+        expect(page).to have_title(title)
+      end
     end
 
-    context "for a sake" do
-      it "has edit link" do
-        buttons = "sake_buttons_#{sake.id}"
-        text = I18n.t("sakes.sake.edit")
-        path = edit_sake_path(sake.id)
-        expect(find(:test_id, buttons)).to have_link(text, href: path)
+    context "when searching" do
+      before do
+        fill_in("text_search", with: "検索中の酒")
+        click_button("submit_search")
+      end
+
+      it "has title with searching words" do
+        title = "検索中の酒 - #{I18n.t('sakes.index.title')} - SAKAZUKI"
+        expect(page).to have_title(title)
       end
     end
   end
