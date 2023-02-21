a55fce1dbcd26b006acf9363357a7a09b275eec8
momocus <momocus333@gmail.com>
2023-02-21 18:37:42 +0900

    Fix: 開発環境では画像の変換を行わない

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,32 +6,25 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  process(ocr: "adv_ocr:ja") if Rails.env.production?
-  process(convert: "webp")
+  if Rails.env.production?
+    process(ocr: "adv_ocr:ja")
+    process(convert: "webp")
+  end
 
   version :thumb do
     # HACK: 1:1でスマホで２つ並んでも潰れないサイズ
-    process(resize_to_fill: [600, 600])
+    process(resize_to_fill: [600, 600]) if Rails.env.production?
   end
 
   version :large_twitter_card do
     if Rails.env.production?
-      cloudinary_transformation(crop: :thumb, width: 600, height: 300, sign_url: true, gravity: :ocr_text)
-    else
-      process(resize_to_fill: [600, 300])
+      cloudinary_transformation(crop: :thumb, width: 600, height: 300, sign_url: true,
+                                gravity: :ocr_text)
     end
   end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def filename
-    if Rails.env.production?
-      super
-    elsif original_filename.present?
-      "#{super.chomp(File.extname(super))}.webp"
-    end
   end
 
   def extension_allowlist
