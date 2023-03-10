690c8a4119022f0615b898165f7093eb5a393af5
SAITOU Keita <keita44.f4@gmail.com>
2023-03-10 14:53:52 +0900

    Update: 酒index/showを新たなTasteGraphに対応した

--- app/javascript/taste_graph/show_taste_graph.ts
+++ app/javascript/taste_graph/show_taste_graph.ts
@@ -1,26 +1,18 @@
 import { DomValues, TasteGraph } from "./taste_graph"
 
 function getDomValues(canvas: HTMLCanvasElement): DomValues {
-  const tasteS = canvas.dataset.tasteValue
-  const aromaS = canvas.dataset.aromaValue
-  // データがないやparse失敗など、Domからデータが取れない場合はNaN,NaNを返す
-  return tasteS && aromaS
-    ? { taste: parseInt(tasteS), aroma: parseInt(aromaS) }
-    : { taste: NaN, aroma: NaN }
-}
-
-const hasCanvasID = (elem: HTMLCanvasElement): boolean => {
-  const id = elem.getAttribute("id")
-  return id != null
+  const taste = canvas.dataset.tasteValue ?? ""
+  const aroma = canvas.dataset.aromaValue ?? ""
+  return { taste, aroma }
 }
 
 // Main
 {
   const canvases = Array.from(document.getElementsByTagName("canvas"))
-  const graphs = canvases.filter(hasCanvasID)
+  const graphCanvases = canvases.filter((c) => c.hasAttribute("id"))
   const config = { pointRadius: 6, zeroLineWidth: 3 }
-  graphs.forEach((canvas) => {
-    const { taste, aroma } = getDomValues(canvas)
-    new TasteGraph(canvas, taste, aroma, config)
+  graphCanvases.forEach((c) => {
+    const dom = getDomValues(c)
+    new TasteGraph(c, dom, config)
   })
 }
