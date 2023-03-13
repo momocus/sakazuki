1d472990df8718dcfd155d3ab5028d8b12374532
SAITOU Keita <keita44.f4@gmail.com>
2023-03-13 17:18:11 +0900

    Update: rails turbo:install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 /* eslint-env node */
+import "@hotwired/turbo-rails"
 
 // eslint-disable-next-line no-unused-vars
 import * as bootstrap from "bootstrap"
