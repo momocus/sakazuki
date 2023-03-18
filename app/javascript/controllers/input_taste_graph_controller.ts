8660cb24008584bb2cbf9f6a5a1c64c157ed05b4
SAITOU Keita <keita44.f4@gmail.com>
2023-03-19 00:50:27 +0900

    Update: 酒フォームでの味香りグラフの入力をStimulus化した

---
+++ app/javascript/controllers/input_taste_graph_controller.ts
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+import { DomValues, TasteGraph } from "../taste_graph/taste_graph"
+
+// Connects to data-controller="input-taste-graph"
+export default class InputTasteGraphController extends Controller<HTMLDivElement> {
+  static targets = ["canvas", "taste", "aroma"]
+  declare readonly canvasTarget: HTMLCanvasElement
+  declare readonly tasteTarget: HTMLInputElement
+  declare readonly aromaTarget: HTMLInputElement
+
+  connect() {
+    const dom: DomValues = {
+      taste: this.tasteTarget.value,
+      aroma: this.aromaTarget.value,
+    }
+    const domCallback = ({ taste, aroma }: Readonly<DomValues>) => {
+      this.tasteTarget.value = taste
+      this.aromaTarget.value = aroma
+    }
+    new TasteGraph({ canvas: this.canvasTarget, dom, domCallback })
+  }
+}
