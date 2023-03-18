1ade1f17c4c9c891786895ad6c623ddb8e03f317
SAITOU Keita <keita44.f4@gmail.com>
2023-03-18 22:22:16 +0900

    Update: 味香りグラフの表示をStimulus化した

---
+++ app/javascript/controllers/show_taste_graph_controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+import { DomValues, TasteGraph } from "../taste_graph/taste_graph"
+
+// Connects to data-controller="show-taste-graph"
+export default class ShowTasteGraphController extends Controller<HTMLCanvasElement> {
+  static values = {
+    taste: String,
+    aroma: String,
+  }
+  declare tasteValue: string
+  declare aromaValue: string
+
+  static targets = ["canvas"]
+  declare readonly canvasTarget: HTMLCanvasElement
+
+  connect() {
+    const dom: DomValues = { taste: this.tasteValue, aroma: this.aromaValue }
+    const config = { pointRadius: 6, zeroLineWidth: 3 }
+    new TasteGraph({ canvas: this.canvasTarget, dom, config })
+  }
+}
