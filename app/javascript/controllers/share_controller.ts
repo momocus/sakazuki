313641112afa914caaa6eb3d2a13b5ccb993c925
SAITOU Keita <keita44.f4@gmail.com>
2023-03-21 18:01:43 +0900

    Update: シェア機能をStimulus化した

---
+++ app/javascript/controllers/share_controller.ts
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="share"
+export default class ShareController extends Controller<HTMLDivElement> {
+  static values = {
+    title: String,
+    url: String,
+  }
+  declare readonly titleValue: string
+  declare readonly urlValue: string
+
+  static targets = ["text", "link"]
+  declare readonly textTarget: HTMLTextAreaElement
+  declare readonly linkTarget: HTMLAnchorElement
+
+  connect() {
+    const data: ShareData = {
+      title: this.titleValue,
+      text: this.textTarget.value,
+      url: this.urlValue,
+    }
+    this.linkTarget.addEventListener("click", () => void navigator.share(data))
+  }
+}
