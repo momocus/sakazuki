75a229687b923727b1c49d31193701ffc261bd27
SAITOU Keita <keita44.f4@gmail.com>
2023-03-25 03:25:26 +0900

    Refactor: simplecovのロードを設定ファイル先頭へ移動した
    ドキュメントにはなるべくトップに置けと書いてある。
    一番早くロードされるrails_helper.rbの先頭へ移動した。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+require "simplecov-cobertura"
+
+SimpleCov.start("rails")
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter if ENV["CI"]
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
