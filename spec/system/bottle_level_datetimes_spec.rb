314d1bfd851cdb1e3c108f7758eabb83e18ea8b5
momocus <momocus333@gmail.com>
2023-04-06 12:26:28 +0900

    Refactor: before処理の刷新
    - タイムスタンプが重要なため、letを使わずにbefore処理で酒を登録するようにした
      - letでsakeを作ると、create_atのタイムスタンプが制御しづらい
    - bottle_levelを変更する時のbefore処理を以下のようにした
      - 酒登録
      - travel_toで7日待つ
      - bottle_levelを変える
    - 酒を登録したりbottle_levelを変えたりする処理を切り出して共通化した

--- spec/system/bottle_level_datetimes_spec.rb
+++ spec/system/bottle_level_datetimes_spec.rb
@@ -9,12 +9,32 @@ RSpec.describe "Bottle Level Datetimes" do
     sign_in(user)
   end
 
+  # 引数で指定したbottle_levelの酒を登録する
+  def create_sake(bottle_level)
+    visit new_sake_path
+    fill_in("sake_name", with: "生道井")
+    select(I18n.t("enums.sake.bottle_level.#{bottle_level}"), from: "sake_bottle_level")
+    click_button("form_submit")
+  end
+
+  # 引数で指定したsakeのbottle_levelを指定した値に更新する
+  def edit_bottle_level(sake, bottle_level)
+    visit edit_sake_path(sake.id)
+    select(I18n.t("enums.sake.bottle_level.#{bottle_level}"), from: "sake_bottle_level")
+    click_button("form_submit")
+  end
+
+  # travel_to でn日後に行く。
+  # するとログインセッションが切れるので、再ログインする。
+  def travel_and_re_sign_in(num)
+    travel_to(num.days.after)
+    visit current_path # ページをリロードしないと再ログインできない
+    sign_in(user)
+  end
+
   context "when creating a new sealed sake" do
     before do
-      visit new_sake_path
-      fill_in("sake_name", with: "生道井")
-      select(I18n.t("enums.sake.bottle_level.sealed"), from: "sake_bottle_level")
-      click_button("form_submit")
+      create_sake("sealed")
     end
 
     describe "created_at" do
@@ -28,10 +48,7 @@ RSpec.describe "Bottle Level Datetimes" do
 
   context "when creating a new opened sake" do
     before do
-      visit new_sake_path
-      fill_in("sake_name", with: "生道井")
-      select(I18n.t("enums.sake.bottle_level.opened"), from: "sake_bottle_level")
-      click_button("form_submit")
+      create_sake("opened")
     end
 
     describe "created_at" do
@@ -53,10 +70,7 @@ RSpec.describe "Bottle Level Datetimes" do
 
   context "when creating a new empty sake" do
     before do
-      visit new_sake_path
-      fill_in("sake_name", with: "生道井")
-      select(I18n.t("enums.sake.bottle_level.empty"), from: "sake_bottle_level")
-      click_button("form_submit")
+      create_sake("empty")
     end
 
     describe "created_at" do
@@ -85,14 +99,11 @@ RSpec.describe "Bottle Level Datetimes" do
   end
 
   context "when opening a sealed sake" do
-    created_at = Time.current
-    let(:sake) { create(:sake, bottle_level: "sealed", created_at:) }
-
     before do
-      travel_to(7.days.after)
-      visit edit_sake_path(sake.id)
-      select(I18n.t("enums.sake.bottle_level.opened"), from: "sake_bottle_level")
-      click_button("form_submit")
+      create_sake("sealed")
+      sake = sake_from_show_path(page.current_path)
+      travel_and_re_sign_in(7)
+      edit_bottle_level(sake, "opened")
     end
 
     describe "created_at" do
@@ -104,7 +115,7 @@ RSpec.describe "Bottle Level Datetimes" do
 
     describe "opened_at" do
       it "is changed to close to now" do
-        opened_at = sake.opened_at
+        opened_at = sake_from_show_path(page.current_path).opened_at
         now = Time.current
         expect(opened_at).to be_within(delta).of(now)
       end
@@ -112,14 +123,11 @@ RSpec.describe "Bottle Level Datetimes" do
   end
 
   context "when empting a sealed sake" do
-    created_at = Time.current
-    let(:sake) { create(:sake, bottle_level: "sealed", created_at:) }
-
     before do
-      travel_to(7.days.after)
-      visit edit_sake_path(sake.id)
-      select(I18n.t("enums.sake.bottle_level.empty"), from: "sake_bottle_level")
-      click_button("form_submit")
+      create_sake("sealed")
+      sake = sake_from_show_path(page.current_path)
+      travel_and_re_sign_in(7)
+      edit_bottle_level(sake, "empty")
     end
 
     describe "created_at" do
@@ -147,15 +155,13 @@ RSpec.describe "Bottle Level Datetimes" do
   end
 
   context "when empting a opened sake" do
-    created_at = 7.days.ago
-    opened_at = Time.current
-    let(:sake) { create(:sake, bottle_level: "opened", created_at:, opened_at:) }
-
     before do
-      travel_to(4.days.after)
-      visit edit_sake_path(sake.id)
-      select(I18n.t("enums.sake.bottle_level.empty"), from: "sake_bottle_level")
-      click_button("form_submit")
+      create_sake("sealed")
+      sake = sake_from_show_path(page.current_path)
+      travel_and_re_sign_in(7)
+      edit_bottle_level(sake, "opened")
+      travel_and_re_sign_in(4)
+      edit_bottle_level(sake, "empty")
     end
 
     describe "created_at" do
