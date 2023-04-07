c39ccc4ec7e6203593ac3d635e6bad681e41e42e
momocus <momocus333@gmail.com>
2023-04-07 22:54:36 +0900

    Fix: travel_toのブロックで過去に戻るようにした
    時間移動が増え、deltaが1秒だと足りなくなったので5秒にした。

--- spec/system/bottle_level_datetimes_spec.rb
+++ spec/system/bottle_level_datetimes_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Bottle Level Datetimes" do
   let(:user) { create(:user) }
 
-  delta = 1.second
+  delta = 5.seconds
 
   # 引数で指定したbottle_levelの酒を登録する
   def create_sake(bottle_level)
@@ -92,9 +92,10 @@ RSpec.describe "Bottle Level Datetimes" do
 
   context "when opening a sealed sake" do
     before do
-      sign_in(user)
-      id = create_sake("sealed")
-      travel_to(7.days.after)
+      id = travel_to(7.days.ago) {
+        sign_in(user)
+        create_sake("sealed")
+      }
       visit(current_path) # ページをリロードしないと再ログインできない
       sign_in(user)
       edit_bottle_level(id, "opened")
@@ -118,9 +119,10 @@ RSpec.describe "Bottle Level Datetimes" do
 
   context "when empting a sealed sake" do
     before do
-      sign_in(user)
-      id = create_sake("sealed")
-      travel_to(7.days.after)
+      id = travel_to(7.days.ago) {
+        sign_in(user)
+        create_sake("sealed")
+      }
       visit(current_path)
       sign_in(user)
       edit_bottle_level(id, "empty")
@@ -152,13 +154,15 @@ RSpec.describe "Bottle Level Datetimes" do
 
   context "when empting a opened sake" do
     before do
-      sign_in(user)
-      id = create_sake("sealed")
-      travel_to(7.days.after)
-      visit(current_path)
-      sign_in(user)
-      edit_bottle_level(id, "opened")
-      travel_to(4.days.after)
+      id = travel_to(7.days.ago) {
+        sign_in(user)
+        create_sake("sealed")
+      }
+      travel_to(4.days.ago) do
+        visit(current_path)
+        sign_in(user)
+        edit_bottle_level(id, "opened")
+      end
       visit(current_path)
       sign_in(user)
       edit_bottle_level(id, "empty")
@@ -167,7 +171,7 @@ RSpec.describe "Bottle Level Datetimes" do
     describe "created_at" do
       it "is not changed" do
         created_at = sake_from_show_path(page.current_path).created_at
-        expect(created_at).to be_within(delta).of(11.days.ago)
+        expect(created_at).to be_within(delta).of(7.days.ago)
       end
     end
 
