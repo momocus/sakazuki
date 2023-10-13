e1ecb0d9b40002c899cbaeb7edba28df0aac36ef
SAITOU Keita <keita44.f4@gmail.com>
2023-10-13 16:19:24 +0900

    Update: letter_openerへのリンクがないテストをprod環境で実行するようにした

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -7,8 +7,14 @@ RSpec.describe "layouts/application", type: :system do
   end
 
   describe "letter_opener link" do
-    it "is not in production and test environment" do
-      expect(page).not_to have_selector(:css, 'a[href="/letter_opener"]')
+    context "when production environment" do
+      before do
+        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+      end
+
+      it "is not in production and test environment" do
+        expect(page).not_to have_selector(:css, 'a[href="/letter_opener"]')
+      end
     end
   end
 end
