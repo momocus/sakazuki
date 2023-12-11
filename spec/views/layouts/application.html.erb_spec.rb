0728cfeaa10ba4c5c80a74f91d4f50d52b21a6b0
SAITOU Keita <keita44.f4@gmail.com>
2023-12-11 17:10:44 +0900

    Fix: テストする環境を変更するコードを先頭に移動した
    ページ描画より前に環境を変更しないと効果を発しない

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 # Capybaraを使うためにsystem specを指定する
 RSpec.describe "layouts/application", type: :system do
-  before do
-    visit root_path
-  end
+  context "when production environment" do
+    before do
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+    end
 
-  describe "letter_opener link" do
-    context "when production environment" do
+    describe "letter_opener link" do
       before do
-        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+        visit root_path
       end
 
       it "does not exist" do
