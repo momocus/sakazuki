11c6e65b0e1c114b67ceccd97c768fa41b5d0a0b
SAITOU Keita <keita44.f4@gmail.com>
2023-12-11 17:09:51 +0900

    Refactor: Rubocopエラーに応じてhave_selectorでcssマッチを使わないようにした
    have_xpathなら同じことをしても許される。
    なお、have_cssで同じことをしようとすると、別なRubocopエラーで怒られる。
    その場合はhave_linkを使えと言われるが、こちらではリンクテキストが必須となってしまう。

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "layouts/application", type: :system do
       end
 
       it "does not exist" do
-        expect(page).not_to have_selector(:css, 'a[href="/letter_opener"]')
+        expect(page).not_to have_xpath('//a[@href="/letter_opener"]')
       end
     end
   end
