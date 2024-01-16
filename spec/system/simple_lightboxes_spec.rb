67be0cbfae23a9af47ede2b2a6e32403a76b1232
SAITOU Keita <keita44.f4@gmail.com>
2024-01-16 17:17:22 +0900

    Refactor: 酒showページをコンテキストにした

--- spec/system/simple_lightboxes_spec.rb
+++ spec/system/simple_lightboxes_spec.rb
@@ -3,31 +3,33 @@ require "rails_helper"
 RSpec.describe "Simple Lightboxes", :js do
   let(:sake) { sake_with_photos(photo_count: 1) }
 
-  before do
-    visit sake_path(sake.id)
-  end
-
-  context "with avif photo" do
-    let(:sake) { sake_with_photos }
-
+  context "with sake show page" do
     before do
-      find(:test_id, "sake_photo").click
+      visit sake_path(sake.id)
     end
 
-    it "has same path" do
-      expect(page).to have_current_path(sake_path(sake.id))
+    context "with avif photo" do
+      let(:sake) { sake_with_photos }
+
+      before do
+        find(:test_id, "sake_photo").click
+      end
+
+      it "has same path" do
+        expect(page).to have_current_path(sake_path(sake.id))
+      end
     end
-  end
 
-  context "with jpg photo" do
-    let(:sake) { sake_with_photos(file_ext: "jpg") }
+    context "with jpg photo" do
+      let(:sake) { sake_with_photos(file_ext: "jpg") }
 
-    before do
-      find(:test_id, "sake_photo").click
-    end
+      before do
+        find(:test_id, "sake_photo").click
+      end
 
-    it "has same path" do
-      expect(page).to have_current_path(sake_path(sake.id))
+      it "has same path" do
+        expect(page).to have_current_path(sake_path(sake.id))
+      end
     end
   end
 end
