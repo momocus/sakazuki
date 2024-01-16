68587ac1b9d1c61404e2324908148b7be303dbfe
SAITOU Keita <keita44.f4@gmail.com>
2024-01-16 17:45:46 +0900

    Add: 酒showページでSimpleLightboxが起動するテストを追加

--- spec/system/simple_lightboxes_spec.rb
+++ spec/system/simple_lightboxes_spec.rb
@@ -4,14 +4,18 @@ RSpec.describe "Simple Lightboxes", :js do
   context "with sake show page" do
     before do
       visit sake_path(sake.id)
+      find(:test_id, "sake_photo").click
     end
 
     context "with avif photo" do
       let(:sake) { sake_with_photos }
 
       it "does not change current path" do
-        find(:test_id, "sake_photo").click
         expect(page).to have_current_path(sake_path(sake.id))
+      end
+
+      it "exists" do
+        expect(page).to have_css(".simple-lightbox")
       end
     end
 
@@ -19,8 +23,11 @@ RSpec.describe "Simple Lightboxes", :js do
       let(:sake) { sake_with_photos(file_ext: "jpg") }
 
       it "does not change current path" do
-        find(:test_id, "sake_photo").click
         expect(page).to have_current_path(sake_path(sake.id))
+      end
+
+      it "exists" do
+        expect(page).to have_css(".simple-lightbox")
       end
     end
   end
