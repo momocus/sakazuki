bfda6eb047ffd4bd585f5de6a3f45e945564d162
SAITOU Keita <keita44.f4@gmail.com>
2024-09-10 14:51:54 +0900

    Update: rails app:updateに従い7.1.4の設定にアップデート
    - rails app:updateコマンドを実行した
      - Rails設定のdiffを確認し、自分たちで設定した項目以外はデフォルトに追従した
    - rails db:migrateを実行した

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module Sakazuki
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults(7.0)
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -30,6 +35,9 @@ module Sakazuki
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
 
     # Rails error forms using Bootstrap is-invalid style
     config.action_view.field_error_proc = lambda { |html_tag, _instance|
