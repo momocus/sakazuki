cc03a34f0b6331a12720baa1b05a24e412bb00e9
SAITOU Keita <keita44.f4@gmail.com>
2024-09-11 19:30:06 +0900

    Fix: 警告「Rails 8.0で削除予定のenumの定義」を修正した
    警告内容は以下のようなもの。
    
    DEPRECATION WARNING: Defining enums with keyword arguments is deprecated and will be removed
    in Rails 8.0. Positional arguments should be used instead

--- app/models/sake.rb
+++ app/models/sake.rb
@@ -44,20 +44,20 @@
 # rubocop:disable Metrics/ClassLength
 class Sake < ApplicationRecord
   has_many :photos, dependent: :destroy
-  enum bottle_level: {
+  enum :bottle_level, {
     sealed: 0,
     opened: 1,
     empty: 2,
   }
-  enum hiire: {
+  enum :hiire, {
     unknown: 0,
     namanama: 1,
     mae_hiire: 2,
     ato_hiire: 3,
     nido_hiire: 4,
     ikkai_hiire: 5,
-  }, _prefix: true
-  enum tokutei_meisho: {
+  }, prefix: true
+  enum :tokutei_meisho, {
     none: 0,
     honjozo: 1,
     ginjo: 2,
@@ -67,19 +67,19 @@ class Sake < ApplicationRecord
     junmai_daiginjo: 6,
     tokubetsu_honjozo: 7,
     tokubetsu_junmai: 8,
-  }, _prefix: true
-  enum moto: {
+  }, prefix: true
+  enum :moto, {
     unknown: 0,
     kimoto: 1,
     yamahai: 2,
     sokujo: 3,
     mizumoto: 4,
-  }, _prefix: true
-  enum warimizu: {
+  }, prefix: true
+  enum :warimizu, {
     unknown: 0,
     kasui: 1,
     genshu: 2,
-  }, _prefix: true
+  }, prefix: true
 
   validates :name, presence: true
   validates :kura, exclusion: { in: [nil] }
