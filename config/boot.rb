c4fd9116a2c402cf7203aee3d905b863ae69b6d5
SAITOU Keita <keita44.f4@gmail.com>
2024-09-18 23:12:50 +0900

    Refactor: RubocopのStyle/RequireOrderエラーを無視した
    Railsのセットアップのため、順番を維持する。

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
+# Railsのセットアップなので順番を変更しない
+# rubocop:disable Style/RequireOrder
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+# rubocop:enable Style/RequireOrder
