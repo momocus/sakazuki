d6509b1fe1caa5c1753d5dc640c0306c7f5d836e
SAITOU Keita <keita44.f4@gmail.com>
2024-12-20 18:24:56 +0900

    Add: SakeSizeControllerにドキュメントを追加

--- app/javascript/controllers/sake_size_controller.ts
+++ app/javascript/controllers/sake_size_controller.ts
@@ -8,15 +8,26 @@ export default class SakeSizeController extends Controller<HTMLDivElement> {
   declare readonly otherSizeTarget: HTMLInputElement
   declare readonly sizeTarget: HTMLInputElement
 
+  /**
+   * その他酒サイズの入力フォームのenable/disableを管理する
+   *
+   * その他radioボタンが選ばれているときは、入力フォームを有効化し値必須にする。
+   */
   updateOtherSizeEnablement(): void {
     this.otherSizeTarget.disabled = !this.otherRadioTarget.checked
     this.otherSizeTarget.required = this.otherRadioTarget.checked
   }
 
+  /** 本当の酒サイズフォームにその他酒サイズを書き込む */
   writeHiddenWithOtherSize(): void {
     this.sizeTarget.value = this.otherSizeTarget.value
   }
 
+  /**
+   * 本当の酒サイズフォームを押されたradioボタンの数値によって書き込む
+   *
+   * @param event - コントローラの呼び出し元イベント
+   */
   writeHiddenByRadio(event: Event): void {
     const elem = event.currentTarget as HTMLInputElement
     const size = elem.dataset.sakeSize
@@ -26,10 +37,12 @@ export default class SakeSizeController extends Controller<HTMLDivElement> {
     else this.sizeTarget.value = size
   }
 
+  /** その他酒サイズフォームに本当の酒サイズを書き込む */
   private writeOtherSizeWithHidden(): void {
     this.otherSizeTarget.value = this.sizeTarget.value
   }
 
+  /** 初期表示時に本当の酒サイズを読み込み、適切なradioにチェックをつける */
   private readHiddenSize(): void {
     const hiddenSize = this.sizeTarget.value
     const matchedRadio = this.radioTargets.find(
