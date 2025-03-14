82362b31a01e31600eff8c86db7a51bcfc31d1a1
SAITOU Keita <keita44.f4@gmail.com>
2025-03-14 11:51:00 +0900

    Refactor: eslint.config.mjsにprettierをかけた

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  // 対象ファイル
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+
+  // ブラウザ環境を有効化
+  { languageOptions: { globals: globals.browser } },
+
+  // ESLintの推奨デフォルト
   pluginJs.configs.recommended,
+
   ...tseslint.configs.recommended,
-];
+]
