5dae894b3672488c4c6b93a30cbb5d87e4281c65
SAITOU Keita <keita44.f4@gmail.com>
2025-03-26 14:12:12 +0900

    Update: zizmorの検出レベルを上げた

--- cli-scripts/run-all-checks.sh
+++ cli-scripts/run-all-checks.sh
@@ -108,13 +108,13 @@ fi
 
 message "##### Run zizmor"
 if type zizmor > /dev/null 2>&1; then
-    zizmor --offline --collect=default .
+    zizmor --offline --persona pedantic --collect default .
 elif type docker > /dev/null 2>&1; then
     docker run --rm --tty \
            --mount type=bind,src="$(pwd)",dst=/repo,readonly \
            --workdir /repo \
            ghcr.io/woodruffw/zizmor:latest \
-           --offline --collect=default .
+           --offline --persona pedantic --collect default .
 else
     warning "[SKIP] zizmor, zizmor or Docker is required."
 fi
