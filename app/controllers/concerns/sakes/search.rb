ea86d2226027b2279ab66e985737d967ee566df7
SAITOU Keita <keita44.f4@gmail.com>
2025-04-28 15:41:08 +0900

    Refactor: 酒検索のためのRansackクエリ―処理をすべてconcern化した

--- app/controllers/concerns/sakes/search.rb
+++ app/controllers/concerns/sakes/search.rb
@@ -8,15 +8,31 @@ module Sakes
 
     private
 
-    def to_multi_search!(query)
-      words = query.delete(:all_text_cont)
-      query[:groupings] = separate_words(words)
+    def initialize_query(query)
+      query ? query.deep_dup : {}
+    end
+
+    # 空き瓶を表示するかどうかを調べる
+    #
+    # @param query [Hash{Symbol => String}] params[:q]に格納されたRansackのクエリ
+    # @return [Boolean] 空き瓶も込みで表示するならtrueを返す
+    def include_empty?(query)
+      !query.nil? and query[:bottle_level_not_eq] == Sake::BOTTOM_BOTTLE.to_s
+    end
+
+    def to_default_bottle!(query)
+      query[:bottle_level_not_eq] = Sake.bottle_levels["empty"]
     end
 
     def separate_words(words)
       # 全角空白または半角空白で区切ることを許可
       # { :name_cont => "" }があり得るがransackがSQL変換で削除するのでOK
       words.split(/[ 　]/).map { |word| { all_text_cont: word } }
+    end
+
+    def to_multi_search!(query)
+      words = query.delete(:all_text_cont)
+      query[:groupings] = separate_words(words)
     end
   end
 end
