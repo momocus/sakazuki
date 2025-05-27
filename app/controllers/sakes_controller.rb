3c0cfdda49d8f5ee8b5f651071b5ae3d92b055cf
SAITOU Keita <keita44.f4@gmail.com>
2025-05-28 00:28:28 +0900

    Document: 酒コントローラーの画像関連メソッドにドキュメントをつけた

--- app/controllers/sakes_controller.rb
+++ app/controllers/sakes_controller.rb
@@ -184,19 +184,31 @@ class SakesController < ApplicationController
                   :size, :price, :rating, photos: [])
   end
 
+  # 写真が追加されているかどうかを判定する
+  #
+  # @return [Boolean] 写真が追加されていればtrueを返す
   def store_photos?
     sake_params[:photos].present?
   end
 
+  # 酒に紐づく写真を保存する
+  #
+  # @note sake_paramsから写真を取り出して、酒に紐づく写真として保存する
   def store_photos
     photos = sake_params[:photos]
     photos&.each { |photo| @sake.photos.create(image: photo) }
   end
 
+  # 写真が削除されているかどうかを判定する
+  #
+  # @return [Boolean] 削除対象の写真があればtrueを返す
   def delete_photos?
     @sake.photos.any? { |photo| params[photo.checkbox_name].present? }
   end
 
+  # 酒に紐づく写真を削除する
+  #
+  # @note 各写真に対応するチェックボックスが「delete」にチェックされていた場合、その写真を削除する
   def delete_photos
     @sake.photos.each do |photo|
       photo.destroy! if params[photo.checkbox_name] == "delete"
