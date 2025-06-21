058e28e5c22150b65429dbe684c291dc2a974f74
SAITOU Keita <keita44.f4@gmail.com>
2025-06-21 23:58:16 +0900

    Fix: ドキュメントの型情報を修正

--- app/helpers/sakes_helper.rb
+++ app/helpers/sakes_helper.rb
@@ -45,7 +45,7 @@ module SakesHelper
 
   # BYの候補を作成する
   #
-  # @return [Array<String, String>] BYセレクタで使う、表示用文字列と日付データ
+  # @return [Array<[String, String]>] BYセレクタで使う、表示用文字列と日付データ
   def by_collection
     collection = by_range.map { |d| [with_japanese_era(d), d.to_s] }
     collection.push([I18n.t("sakes.form_abstract.unknown"), ""])
@@ -71,7 +71,7 @@ module SakesHelper
   #
   # 現在の日付から30年分を生成する
   #
-  # @return [Array<String, String>] 製造年月セレクタで使う、表示用文字列と日付データ
+  # @return [Array<[String, String]>] 製造年月セレクタで使う、表示用文字列と日付データ
   def bindume_collection
     month_range.map { |d|
       year = with_japanese_era(d)
