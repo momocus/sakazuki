da35df243a28b57dfeb63eadbda9cb26e9292522
SAITOU Keita <keita44.f4@gmail.com>
2025-07-22 17:59:40 +0900

    Add: 酒indexの相対日時にマウスオーバーで絶対日時を追加

--- app/helpers/sakes_helper.rb
+++ app/helpers/sakes_helper.rb
@@ -183,4 +183,23 @@ module SakesHelper
     ml = Sake.alcohol_stock(include_empty:)
     to_shakkan(ml)
   end
+
+  # 酒の最終更新日を現在日時との差に応じて適切な日付フォーマットで返す
+  #
+  # 同じ年なら短縮形式、違う年なら標準形式を表示する。
+  # Railsのデフォルトi18nキーを使用する。
+  #
+  # @param sake [Sake] 酒オブジェクト
+  # @return [String] フォーマットされた日付文字列
+  def absolute_date(sake)
+    date = latest_at(sake).to_date
+    current_year = Time.current.year
+    date_year = date.year
+
+    if current_year == date_year
+      I18n.l(date, format: :short)
+    else
+      I18n.l(date, format: :default)
+    end
+  end
 end
