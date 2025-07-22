7fa9fe9f19b4bc82117e2acbd539b11c6afb75ad
SAITOU Keita <keita44.f4@gmail.com>
2025-07-23 00:16:17 +0900

    Fix: メモリリークを修正
    DOMに結びつけるStimulusは、connectでnewしたら、disconnectでdisposeが必要。

--- app/javascript/controllers/tooltip_controller.ts
+++ app/javascript/controllers/tooltip_controller.ts
@@ -3,7 +3,16 @@ import { Tooltip } from "bootstrap"
 
 // Connects to data-controller="tooltip"
 export default class TooltipController extends Controller<HTMLSpanElement> {
+  private tooltip: Tooltip | null = null
+
   connect() {
-    new Tooltip(this.element)
+    this.tooltip = new Tooltip(this.element)
+  }
+
+  disconnect() {
+    if (this.tooltip !== null) {
+      this.tooltip.dispose()
+      this.tooltip = null
+    }
   }
 }
